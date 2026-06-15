d422c8308268f74354a1009d2d795d4109c3b460
Yuva Punnam <venkatgowrish07@gmail.com>
2026-06-15 10:48:44 +0530

    Update 03_instrmem.v

--- RTL_design/03_instrmem.v
+++ RTL_design/03_instrmem.v
@@ -2,18 +2,18 @@ module instr_memo(
     input [31:0] pc,
     output reg [31:0] instr
 );
-    reg [31:0] mem [0:31];
+
+    reg [31:0] memory[0:31];
+
     initial begin
-        mem[0] = 32'h00B00093; //addi x1,x0,11
-        mem[1] = 32'h01600113; //addi x2,x0,22
-        mem[2] = 32'h001101B3; //addi x3,x2,x1
-        mem[3] = 32'h40110233; //sub x4,x2,x1
-        mem[4] = 32'h00500293; //addi x5,x0,5
-        mem[5] = 32'h00E00313; //addi x6,x0,14
-        mem[6] = 32'h005343B3; //xor x7,x6,x5
-        mem[7] = 32'h00129413; //slli x8,x5,1
-        mem[8] = 32'h000004e7; //jalr x9,x0,0
+        memory[0] = 32'h00100093; // addi x1,x0,1
+        memory[1] = 32'h00000113; // addi x2,x0,0
+        memory[2] = 32'h002081B3; // add  x3,x1,x2
+        memory[3] = 32'h00008133; // add  x2,x1,x0
+        memory[4] = 32'h000180B3; // add  x1,x3,x0
+        memory[5] = 32'hFF5FF06F; // jal  x0,loop
     end
+
     always @(*) instr <= memory[pc[6:2]];
 
 endmodule
